a8ab4ffd2c6e52dfd81a73376e18c484e973f1b3
Aleksandr Danilenko <rorik302@gmail.com>
2025-05-23 23:09:19 +0300

    Добавлены горячие клавиши neovim

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -8,8 +8,10 @@ map("<Space>", "<Nop>", {}, { "n", "v" })
 
 -- Операции и работа с буфером
 map("x", '"_x', { desc = "Delete without yank" }, { "n", "v" })
-map("d", '"_d', { desc = "Delete without yank" }, { "n", "v" })
-map("p", '"_dP', { desc = "Paste without yank" }, { "n", "v" })
+-- map("d", '"_d', { desc = "Delete without yank" }, { "n", "v" })
+map("<leader>d", [["_d]], { desc = "Delete without yank" }, { "n", "v" })
+map("<leader>p", [["_dP]], { desc = "Paste without yank" }, "x")
+map("p", '"_dP', { desc = "Paste without yank" }, "v")
 
 -- Коммандная строка
 function OpenCommandLine()
@@ -25,7 +27,9 @@ map("<leader>w", ":w<CR>", { desc = "Save" })
 map("<leader>q", ":q<CR>", { desc = "Quit" })
 map("<leader>Q", ":qa!<CR>", { desc = "Quit!" })
 
--- Сдвиг текста с сохранением выделения
+-- Сдвиг текста
+map("K", ":move '<-2<CR>gv=gv", { desc = "Move Text Up" }, "v")
+map("J", ":move '>+1<CR>gv=gv", { desc = "Move Text Down" }, "v")
 map(">", ">gv", { desc = "Move Text Right" }, "v")
 map("<", "<gv", { desc = "Move Text Left" }, "v")
 
@@ -40,6 +44,12 @@ map("<A-h>", "<C-w><", { desc = "Window: Resize Left" })
 map("<A-j>", "<C-w>-", { desc = "Window: Resize Down" })
 map("<A-k>", "<C-w>+", { desc = "Window: Resize Up" })
 map("<A-l>", "<C-w>>", { desc = "Window: Resize Right" })
+
+-- Центрирование
+map("<C-d>", "<C-d>zz", { desc = "Scroll: Half Page Down" })
+map("<C-u>", "<C-u>zz", { desc = "Scroll: Half Page Up" })
+map("n", "nzzzv", { desc = "Search: Next" })
+map("N", "Nzzzv", { desc = "Search: Previous" })
 
 -- UI
 map("<leader>uw", ":set wrap!<CR>", { desc = "Toggle Wrap" })
