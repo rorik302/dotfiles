f6510201099dc8db34b503109742588f5b0baed2
Aleksandr Danilenko <rorik302@gmail.com>
2025-05-25 15:48:03 +0300

    Добавлена подсветка цветов

--- .config/nvim/lua/plugins/ccc.lua
+++ .config/nvim/lua/plugins/ccc.lua
@@ -1,7 +1,14 @@
 return {
 	"uga-rosa/ccc.nvim",
 	config = function()
-		require("ccc").setup()
+		local ccc = require("ccc")
+
+		ccc.setup({
+			highlighter = {
+				auto_enable = true,
+				lsp = true,
+			},
+		})
 
 		local map = require("utils").map
 		map("<leader>cc", ":CccPick<CR>", { desc = "Pick color" })
