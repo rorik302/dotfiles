373a87b1e0cd16c5b48613f3ecb00a7de6581e49
Aleksandr Danilenko <rorik302@gmail.com>
2025-06-14 20:27:43 +0300

    Yazi: Добавлены плагины

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -12,3 +12,26 @@ th.git.untracked_sign = ""
 th.git.ignored_sign = ""
 th.git.updated_sign = "!"
 require("git"):setup()
+
+require("projects"):setup({
+	save = {
+		method = "lua", -- yazi | lua
+		-- lua_save_path = "", -- comment out to get the default value
+		-- windows: "%APPDATA%/yazi/state/projects.json"
+		-- unix: "~/.local/state/yazi/projects.json"
+	},
+	last = {
+		update_after_save = true,
+		update_after_load = true,
+		load_after_start = true,
+	},
+	merge = {
+		quit_after_merge = false,
+	},
+	notify = {
+		enable = true,
+		title = "Projects",
+		timeout = 3,
+		level = "info",
+	},
+})
