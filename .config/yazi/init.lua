eefd2b49c7ad24b2ae828fd73c2eab96dde0c70f
Aleksandr Danilenko <rorik302@gmail.com>
2025-07-03 22:56:56 +0300

    Обновлены настройки yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,7 +1,5 @@
----@diagnostic disable: undefined-global
-require("full-border"):setup({
-	type = ui.Border.ROUNDED,
-})
+-- Plugins
+require("full-border").setup()
 
 -- Git
 th.git = th.git or {}
@@ -13,25 +11,29 @@ th.git.ignored_sign = ""
 th.git.updated_sign = "!"
 require("git"):setup()
 
-require("projects"):setup({
-	save = {
-		method = "lua", -- yazi | lua
-		-- lua_save_path = "", -- comment out to get the default value
-		-- windows: "%APPDATA%/yazi/state/projects.json"
-		-- unix: "~/.local/state/yazi/projects.json"
-	},
-	last = {
-		update_after_save = true,
-		update_after_load = true,
-		load_after_start = true,
-	},
-	merge = {
-		quit_after_merge = false,
-	},
-	notify = {
-		enable = true,
-		title = "Projects",
-		timeout = 3,
-		level = "info",
-	},
-})
+-- Add symlink to statusbar
+Status:children_add(function(self)
+	local h = self._current.hovered
+	
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+-- Show user/group in statusbar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	})
+end, 500, Status.RIGHT)
