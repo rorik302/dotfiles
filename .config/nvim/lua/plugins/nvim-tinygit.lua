cb61215149058e421e68a31f7989d72b6eed37cc
Aleksandr Danilenko <rorik302@gmail.com>
2025-07-12 15:43:45 +0300

    Config from scratch

--- .config/nvim/lua/plugins/nvim-tinygit.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"chrisgrieser/nvim-tinygit",
-	config = function()
-		require("tinygit").setup()
-	end,
-}
--- .config/nvim/lua/plugins/search-replace.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("n", "<leader>ro", ":SearchReplaceSingleBufferOpen<CR>", { desc = "Replace: In Open Buffer" })
-vim.keymap.set("n", "<leader>rw", ":SearchReplaceSingleBufferCWord<CR>", { desc = "Replace: Word" })
-
-return {
-	"roobert/search-replace.nvim",
-	config = function()
-		require("search-replace").setup()
-	end,
-}
