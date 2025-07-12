4a582eb6cb3e9a04abaddb6c10121b582d821c99
Aleksandr Danilenko <rorik302@gmail.com>
2025-07-12 23:26:31 +0300

    Обновлены настройки fish, ghostty, hyprland

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,25 @@ if status is-interactive
 end
 
 set -U fish_greeting
+
+set -gx EDITOR nvim
+
+starship init fish | source
+
+# fzf
+fzf --fish | source
+set -g FZF_DEFAULT_OPTS "--exact --ignore-case --style full --preview 'bat -n --color=always {}'"
+
+# zoxide
+zoxide init fish | source
+
+# atuin
+atuin init fish | source
+
+# navi
+navi widget fish | source
+alias navi="navi --print | wl-copy"
+
+# eza
+alias ls="eza --long --all --header --color=always --icons --group-directories-first --git-repos --total-size"
+alias tree="eza --tree -all --header --icons --color=always --group-directories-first --git-repos --total-size"
